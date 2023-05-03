b76cbf41ffde007be3aa369b8afa68541a6a09e1
Vanessa Rubiera <83053924+VanessaRubiera@users.noreply.github.com>
2023-05-02 20:24:04 -0700

    First commit añadiendo modulos y paquetes

---
+++ README.md
@@ -0,0 +1,7 @@
+# MODULOS Y PAQUETES EN PYTHON 📦🐍
+
+* ***Modulo***: `Script` o archivo con extension .py, que contiene codigo de python
+
+* ***Paquete***: Es una `carpeta` que contiene *código seccionado*, ayuda a organizar mejor el código
+
+> ***Nota***:  `__pycache__` almacena los binarios de codigo importado en python, es un algoritmo interno que tiene python. Es para mejorar el performance. Cuando se ejecuta el codigo de un script y tiene import, python interpreta los script y crea `__pycache__`
---
+++ main.py
@@ -0,0 +1,9 @@
+# Tambien se puede importar asi -> import calculator.suma
+# Módulo: Script o archivo con extension .py, contiene codigo de python
+# Paquete: Es una carpeta que contiene código seccionado, ayuda a organizar mejor el código
+# __pycache__: Almacena los binarios de codigo importado en python, es un algoritmo interno que tiene python. Es para mejorar el performance.
+# Cuando se ejecuta el codigo de un script y tiene import, python interpreta los script y crea __pycache__
+
+import modulo
+import calculator.suma
+import calculator.resta
